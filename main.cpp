095eb69a506f6173af3293a1cf7b2db1c8dabb2e
Andrey <andreybr2002@hotmail.com>
2023-10-03 20:34:05 -0300

    Implementação criptografia básica

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
 #define STB_IMAGE_IMPLEMENTATION
+#include "/home/aluno/Desktop/andrey/esteganografia/stb-master/stb_image.h" // Caminho do Linux
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "/home/aluno/Desktop/andrey/esteganografia/stb-master/stb_image_write.h" // Caminho do Linux
 
 // Substituir pelo caminho do computador que vai executar o projeto
-#include "/home/aluno/Desktop/andrey/stb-master/stb_image.h"
-// Caminho do Linux
 
 template <typename T>
 inline T extract_bits(const T v, const unsigned bstart, const unsigned blength) {
@@ -20,43 +22,53 @@ inline T set_bits(const T v, const unsigned bstart, const unsigned blength, cons
 }
 
 int main() {
-   const char *filename = "./imagens/indoali.bmp";
-   int width, height, channels;
+    const char *filename = "./imagens/indoali.bmp";
+    int width, height, channels;
 
-   // Carregar a imagem usando STB Image
-   unsigned char *image_data = stbi_load(filename, &width, &height, &channels, 0);
+    // Carregar a imagem
+    unsigned char *image_data = stbi_load(filename, &width, &height, &channels, 0);
 
-   if (!image_data) {
-      printf("Erro ao abrir imagem: %s\n", filename);
-      return 1;
-   }
+    if (!image_data) {
+        printf("Erro ao abrir imagem: %s\n", filename);
+        return 1;
+    }
 
-   unsigned int red_pixels = 0;
-   unsigned int green_pixels = 0;
-   unsigned int blue_pixels = 0;
+    // Texto que você deseja esconder na imagem
+    const char *texto = "Mensagem secreta";
 
-   // Iterar sobre os pixels e contar os que têm componente de cada cor maior ou igual a 111
-   for (int i = 0; i < width * height * channels; i += channels) {
-      unsigned char red = image_data[i];
-      unsigned char green = image_data[i + 1]; // Canal verde
-      unsigned char blue = image_data[i + 2];  // Canal azul
+    int texto_len = strlen(texto);
+    int texto_index = 0;
 
-      if (red >= 111)
-         red_pixels++;
+    // Iterar sobre os pixels e esconder o texto nos canais vermelho e verde
+    for (int i = 0; i < width * height * channels && texto_index < texto_len; i += channels) {
+        unsigned char red = image_data[i];
+        unsigned char green = image_data[i + 1];
+        //unsigned char blue = image_data[i + 2];
 
-      if (green >= 111)
-         green_pixels++;
+        if (texto_index < texto_len) {
+            char caractere = texto[texto_index];
+            unsigned char metade1 = extract_bits(caractere, 0, 4);
+            unsigned char metade2 = extract_bits(caractere, 4, 4);
 
-      if (blue >= 111)
-         blue_pixels++;
-   }
+            // Definir metade1 nos bits menos significativos do canal vermelho
+            red = set_bits(red, 0, 4, metade1);
 
-   // Liberar a memória alocada para a imagem
-   stbi_image_free(image_data);
+            // Definir metade2 nos bits menos significativos do canal verde
+            green = set_bits(green, 0, 4, metade2);
 
-   printf("Numero de pixels vermelhos >= 111: %d\n", red_pixels);
-   printf("Numero de pixels verdes >= 111: %d\n", green_pixels);
-   printf("Numero de pixels azuis >= 111: %d\n", blue_pixels);
+            // Atualizar os valores dos canais vermelho e verde no pixel
+            image_data[i] = red;
+            image_data[i + 1] = green;
+        }
 
-   return 0;
+        texto_index++;
+    }
+
+    // Salvar a imagem criptografada e liberar o espaço alocado na memória
+    stbi_write_bmp("imagem_com_mensagem.bmp", width, height, channels, image_data);
+    stbi_image_free(image_data);
+
+    printf("Mensagem escondida na imagem.\n");
+
+    return 0;
 }
